91db63eefa6727037d222bb77a4d0af43385b231
Jean-Hadrien Chabran <jh@chabran.fr>
2021-12-04 20:42:30 +0100

    Add a command to check for broken links (#1)

---
+++ cmd/checks/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"regexp"
+	"time"
+)
+
+type brokenLinkErr error
+
+func main() {
+	links, err := extractLinks(os.Args[1])
+	if err != nil {
+		log.Fatalf("cannot parse links: %q", err)
+	}
+
+	brokenLinks, err := checkLinks(links)
+	if err != nil {
+		log.Fatalf("cannot check links: %q", err)
+	}
+
+	if len(brokenLinks) > 0 {
+		for _, err := range brokenLinks {
+			fmt.Printf("❌%s\n", err.Error())
+		}
+		fmt.Printf("\033[1mFound %d errors.\033[0m\n", len(brokenLinks))
+		os.Exit(1)
+	}
+}
+
+func extractLinks(filepath string) ([]string, error) {
+	f, err := os.Open(filepath)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	links := []string{}
+	re := regexp.MustCompile(`\((https?://[^\s]+)\)`)
+	s := bufio.NewScanner(f)
+	for s.Scan() {
+		line := s.Text()
+		matches := re.FindStringSubmatch(line)
+		if len(matches) > 1 {
+			links = append(links, matches[1])
+		}
+	}
+	if err := s.Err(); err != nil {
+		return nil, err
+	}
+
+	return links, nil
+}
+
+func checkLinks(links []string) ([]brokenLinkErr, error) {
+	brokenLinkErrs := []brokenLinkErr{}
+
+	c := &http.Client{
+		Timeout: time.Second * 10, // be lenient
+	}
+	for _, link := range links {
+		resp, err := c.Get(link)
+		if err != nil {
+			brokenLinkErrs = append(brokenLinkErrs, fmt.Errorf("ERR: %q, %s", err, link))
+			continue
+		}
+		if resp.StatusCode >= 400 {
+			brokenLinkErrs = append(brokenLinkErrs, fmt.Errorf("%d: %q", resp.StatusCode, link))
+		}
+	}
+	return brokenLinkErrs, nil
+}
